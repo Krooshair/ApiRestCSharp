8fbb90299af02dcdef145e8c9fe675b08e9fb157
Fabian Bermudez <eddybermudez200103@gmail.com>
2024-06-13 22:31:00 -0500

    Agregar archivos de proyecto.

---
+++ PrimeraApiRest/Controllers/ClienteController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeraApiRest.Models;
+
+namespace PrimeraApiRest.Controllers
+{
+    [ApiController]
+    [Route("cliente")]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public dynamic listarCliente()
+        {
+
+            List<Cliente> clientes = new List<Cliente>
+            {
+                new Cliente {
+                    nombre = "Fabian",
+                    edad = "20"
+                },
+                new Cliente {
+                    nombre = "Roler",
+                    edad = "44"
+                },
+                new Cliente {
+                    nombre = "Christopher",
+                    edad = "23"
+                }
+            };
+
+            return clientes;
+        }
+
+        [HttpGet]
+        [Route("id")]
+        public dynamic listarClientexId(string _name)
+        {
+            return new Cliente
+            {
+                nombre = "Fabian",
+                edad = "20"
+            };
+        }
+
+        [HttpPost]
+        [Route("")]
+        public dynamic guardarCliente(Cliente cliente)
+        {
+            return new
+            {
+                success = true,
+                message = "Cliente registrado",
+                result = cliente
+            };
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public dynamic eliminarCliente(Cliente cliente)
+        {
+            string token = Request.Headers.Where(x => x.Key == "Authorization").FirstOrDefault().Value;
+
+            if(token != "fabian123.")
+            {
+                return Unauthorized(new
+                {
+                    success = false,
+                    message = "Token no valido",
+                    result = ""
+                });
+            }
+
+            return new
+            {
+                success = true,
+                message = "Cliente eliminado",
+                result = cliente
+            };
+        }
+    }
+}
---
+++ PrimeraApiRest/Models/Cliente.cs
@@ -0,0 +1,8 @@
+﻿namespace PrimeraApiRest.Models
+{
+    public class Cliente
+    {
+        public string nombre {  get; set; }
+        public string edad {  get; set; }
+    }
+}
